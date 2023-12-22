4acb5581ad90a4cd4765fb45f72ee555004d22ba
Rasikrr <rs.t.95@mail.ru>
2023-12-22 13:24:32 +0600

    getSum method in Calculation class wasfixed

--- main/helpers/Calculation.java
+++ main/helpers/Calculation.java
@@ -19,7 +19,7 @@ public class Calculation {
 
     public static double getSum(double[] arr) {
         double sum = 0;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
 
